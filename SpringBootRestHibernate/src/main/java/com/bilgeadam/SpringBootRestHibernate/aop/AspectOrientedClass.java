5b6487aa3d052ab4523ad2e49ef0e6bb788904fe
guderhasan <68234046+guderhasan@users.noreply.github.com>
2022-09-26 20:07:17 +0300

    hasanın değişikliği

--- SpringBootRestHibernate/src/main/java/com/bilgeadam/SpringBootRestHibernate/aop/AspectOrientedClass.java
+++ SpringBootRestHibernate/src/main/java/com/bilgeadam/SpringBootRestHibernate/aop/AspectOrientedClass.java
@@ -13,82 +13,82 @@ package com.bilgeadam.SpringBootRestHibernate.aop;
 //import org.aspectj.lang.annotation.Before;
 //import org.aspectj.lang.annotation.Pointcut;
 //import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.After;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Before;
-//import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+ @Aspect
+ @Component
+ cross cutting concern
+public class AspectOrientedClass
+{
+	//github için örnek
+	 @Pointcut(value = "execution(public * *(..))")
 //
-// @Aspect
-// @Component
-// cross cutting concern
-//public class AspectOrientedClass
-//{
-//	//github için örnek
-//	 @Pointcut(value = "execution(public * *(..))")
-////
-////	 all implementations of interface
-////	 within(JPARepositoryImpl+)
-//
-//	@PostConstruct
-//	public void asd()
-//	{
-//		System.err.println("Aspect sınıfı new yapıldı");
-//	}
-//
-//	// case sensitive
-//	@Before(value = "execution(* com.bilgeadam.SpringBootRestHibernate.controller.OgrenciController.*get*(..))")
-//	public void icindeGetGecenler()
-//	{
-//		System.err.println("Metod adı içinde get geçiyor");
-//	}
-//
-//	@Before(value = "within(com.bilgeadam.SpringBootRestHibernate.controller.*)")
-//	private void allMethodsInsideControllerPackageBefore()
-//	{
-//		System.err.println("Before all methods of all classes of com.bilgeadam.SpringBootRestHibernate.controller package");
-//	}
-//
-//	// bu metod ogretmen controller 'da herhangi bir metod çalıştığında tetiklenecek
-//	@Pointcut(value = "execution(* com.bilgeadam.SpringBootRestHibernate.controller.OgretmenController.*(..))")
-//	public void ogretmenControllerCalisti()
-//	{
-//		// burası çalışmaz
-//		System.err.println("Öğretmen controller 'dan bir metod çalıştı");
-//	}
-//
-//	// kesişim noktasının hem öncesi hem sonrası
-//	@Around(value = "ogretmenControllerCalisti()")
-//	public Object aroundOgretmenControllerCalisti(ProceedingJoinPoint point)
-//	{
-//		System.err.println("Around Öğretmen controller 0");
-//		Object returnValue = null;
-//		try
-//		{
-//			// before after burada çalışıyor
-//			long startTime = System.currentTimeMillis();
-//			returnValue = point.proceed();
-//			long endtime = System.currentTimeMillis();
-//			System.err.println("Class Name: " + point.getSignature().getDeclaringTypeName() + ". Method Name: " + point.getSignature().getName() + ". Time taken for Execution is : " + (endtime - startTime) + "ms");
-//		}
-//		catch (Throwable e)
-//		{
-//		}
-//		System.err.println("Around Öğretmen controller 1");
-//		return returnValue;
-//	}
-//
-//	// kesişim noktasına gelmeden önce
-//	@Before(value = "ogretmenControllerCalisti()")
-//	public void beforeOgretmenControllerCalisti(JoinPoint jp)
-//	{
-//		System.err.println("Öğretmen controller 'dan bir metod çalışacak " + jp.getSignature().getName());
-//	}
-//
-//	// kesişim noktasından sonra
-//	@After(value = "ogretmenControllerCalisti()")
-//	public void afterOgretmenControllerCalisti(JoinPoint jp)
-//	{
-//		System.err.println("Öğretmen controller 'dan bir metod çalıştı " + jp.getSignature().getName());
-//	}
-//}
+//	 all implementations of interface
+//	 within(JPARepositoryImpl+)
+
+	@PostConstruct
+	public void asd()
+	{
+		System.err.println("Aspect sınıfı new yapıldı");
+	}
+
+	// case sensitive
+	@Before(value = "execution(* com.bilgeadam.SpringBootRestHibernate.controller.OgrenciController.*get*(..))")
+	public void icindeGetGecenler()
+	{
+		System.err.println("Metod adı içinde get geçiyor");
+	}
+
+	@Before(value = "within(com.bilgeadam.SpringBootRestHibernate.controller.*)")
+	private void allMethodsInsideControllerPackageBefore()
+	{
+		System.err.println("Before all methods of all classes of com.bilgeadam.SpringBootRestHibernate.controller package");
+	}
+
+	// bu metod ogretmen controller 'da herhangi bir metod çalıştığında tetiklenecek
+	@Pointcut(value = "execution(* com.bilgeadam.SpringBootRestHibernate.controller.OgretmenController.*(..))")
+	public void ogretmenControllerCalisti()
+	{
+		// burası çalışmaz
+		System.err.println("Öğretmen controller 'dan bir metod çalıştı");
+	}
+
+	// kesişim noktasının hem öncesi hem sonrası
+	@Around(value = "ogretmenControllerCalisti()")
+	public Object aroundOgretmenControllerCalisti(ProceedingJoinPoint point)
+	{
+		System.err.println("Around Öğretmen controller 0");
+		Object returnValue = null;
+		try
+		{
+			// before after burada çalışıyor
+			long startTime = System.currentTimeMillis();
+			returnValue = point.proceed();
+			long endtime = System.currentTimeMillis();
+			System.err.println("Class Name: " + point.getSignature().getDeclaringTypeName() + ". Method Name: " + point.getSignature().getName() + ". Time taken for Execution is : " + (endtime - startTime) + "ms");
+		}
+		catch (Throwable e)
+		{
+		}
+		System.err.println("Around Öğretmen controller 1");
+		return returnValue;
+	}
+
+	// kesişim noktasına gelmeden önce
+	@Before(value = "ogretmenControllerCalisti()")
+	public void beforeOgretmenControllerCalisti(JoinPoint jp)
+	{
+		System.err.println("Öğretmen controller 'dan bir metod çalışacak " + jp.getSignature().getName());
+	}
+
+	// kesişim noktasından sonra
+	@After(value = "ogretmenControllerCalisti()")
+	public void afterOgretmenControllerCalisti(JoinPoint jp)
+	{
+		System.err.println("Öğretmen controller 'dan bir metod çalıştı " + jp.getSignature().getName());
+	}
+}
